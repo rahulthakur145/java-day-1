5ff8c32b02833a02c1aba0273c5a4ad4c13c24f0
rahulthakur145 <homemademe145@gmail.com>
2026-01-18 22:22:35 +0530

    First commit

---
+++ day2arrya.java
@@ -0,0 +1,12 @@
+
+public class day2arrya {
+    public static void main(String args[]){
+        char name[] = {'r', 'a', 'h', 'u'};
+        System.out.println(name);
+
+        for(int i=name.length-1; i >= 0; i--){
+            System.out.print(name[i]);
+        }
+    }
+}
+
