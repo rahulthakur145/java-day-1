10182903bb0b15ad59e3d2e59c1cf3c5ac9e647e
rahulthakur145 <homemademe145@gmail.com>
2026-01-18 22:57:23 +0530

    https://github.com/rahulthakur145/java-day-1

---
+++ Assinment1_1.java
@@ -0,0 +1,13 @@
+public class Assinment1_1 {
+    public static void main(String[] args) {
+        String name = "Rahul Thakur";
+        String PRN = "24070122149";
+        String Address = "janakpur ,Nepal;";
+        System.out.println("Name: " + name);
+        System.out.println("PRN:" + PRN);
+        System.out.println("Address: " + Address);
+    }
+}
+
+    
+
