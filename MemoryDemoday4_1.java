7a1bedc8e655c69f1a761de7bfe96aa9eee94782
rahulthakur145 <homemademe145@gmail.com>
2026-01-31 01:21:06 +0530

    https://github.com/rahulthakur145/java-day-1

---
+++ MemoryDemoday4_1.java
@@ -0,0 +1,11 @@
+public class MemoryDemoday4_1 {
+    int x;
+    static int y;
+    public static void main(String[] args) {
+        int z = 10;
+        int x = 5;
+        z = 15;
+        int sum = x + z;
+        System.out.println(sum);
+    }
+}
