90f484401b90d3387142fbc1d76ca6d5d4d814f0
rahulthakur145 <homemademe145@gmail.com>
2026-01-31 01:39:56 +0530

    https://github.com/rahulthakur145/java-day-1

--- day3_3.java
+++ day3_3.java
@@ -1,15 +1,17 @@
-public static void main(String[] args) {
-     class counter{
-        static int count = 0;
-        counter(){
-            count++;
-            System.out.println("object created. current count:" + count);
-
-        }
-
+class counter {
+    static int count = 0;
+    counter() {
+        count++;
+        System.out.println("object created. current count:" + count);
     }
-    new counter();
-    new counter();
-    new counter();
-    System.out.println("final count: " + counter.count);
 }
+
+public class day3_3 {
+    public static void main(String[] args) {
+        counter obj1 = new counter();
+        counter obj2 = new counter();
+        counter obj3 = new counter();
+        System.out.println("final count: " + counter.count);
+    }
+}
+   
